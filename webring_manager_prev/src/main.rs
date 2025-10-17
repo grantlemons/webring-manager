d7f380b198342b715d8114e2d12d23683122260e
Grant Lemons <grantlemons@aol.com>
2025-10-16 23:23:38 -0600

    make much more robust and move all actual logic into new crate

--- webring_manager_prev/src/main.rs
+++ webring_manager_prev/src/main.rs
@@ -1,87 +1,14 @@
-use lambda_http::{
-    http::{header::REFERER, StatusCode, Uri},
-    run, service_fn, Body, Error, Request, RequestExt, Response,
-};
-
-fn get_sites() -> Vec<String> {
-    std::env::var("SITES")
-        .unwrap()
-        .split(" ")
-        .map(|s| s.to_owned())
-        .collect()
-}
-
-async fn function_handler(sites: Vec<String>, event: Request) -> Result<Response<Body>, Error> {
-    let referer = if let Some(referer_header_value) = event.headers().get(REFERER) {
-        referer_header_value.to_str()?.to_owned()
-    } else {
-        event
-            .query_string_parameters()
-            .first("Referer")
-            .unwrap_or_default()
-            .to_owned()
-    }
-    .parse::<Uri>()?
-    .host()
-    .unwrap()
-    .to_owned();
-
-    let referer_index = sites
-        .iter()
-        .position(|s| s.parse::<Uri>().unwrap().host().unwrap() == referer)
-        .unwrap_or_default() as isize;
-
-    let next_index = (referer_index - 1).rem_euclid(sites.len() as isize) as usize;
-    let next_site = sites.get(next_index).unwrap().to_owned();
-
-    let response = Response::builder()
-        .header("Location", next_site)
-        .status(StatusCode::SEE_OTHER)
-        .body("Referring to previous site in webring!".into())?;
-
-    Ok(response)
-}
+use lambda_http::{run, service_fn, tower::BoxError};
+use webring::*;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    run(service_fn(|ev| function_handler(get_sites(), ev))).await
-}
+async fn main() -> Result<(), BoxError> {
+    lambda_http::tracing::init_default_subscriber();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use lambda_http::http::Request;
+    let dec = |x| x - 1;
 
-    async fn abstraction(sites: &[String], referer: &str, location: &str) {
-        let request = Request::builder()
-            .header(REFERER, referer)
-            .body("".into())
-            .unwrap();
-        assert_eq!(
-            function_handler(sites.to_vec(), request)
-                .await
-                .unwrap()
-                .headers()
-                .get("Location")
-                .unwrap(),
-            location
-        )
-    }
-
-    #[tokio::test]
-    async fn test_referer_prev() {
-        let sites = [
-            "https://lukaswerner.com/",
-            "https://grantlemons.com/",
-            "https://elijahpotter.dev/",
-            "https://b-sharman.dev/",
-        ]
-        .iter()
-        .map(|s| s.to_string())
-        .collect::<Vec<_>>();
-
-        for w in sites.windows(2) {
-            abstraction(&sites, &w[1], &w[0]).await
-        }
-    }
+    run(service_fn(|ev| {
+        build_response(calc_destination(extract_referrer(ev), &sitelist(), dec))
+    }))
+    .await
 }
